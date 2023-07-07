7fc7466ddfcc4c688bc8084dfd33048b31837668
Jose Luis Koller <joseluiskoller98@gmail.com>
2023-07-07 14:26:17 -0300

    Test

---
+++ carrer-path/src/app/bst/especialista/page.js
@@ -0,0 +1,129 @@
+"use client";
+import React from "react";
+import { Timeline, Card, Descriptions, Table, Collapse } from "antd";
+import desc from "./desc.json";
+import matrix from "./matrix.json";
+import capa from "./capacities.json";
+
+const columns_desc = [
+  {
+    title: "Competencia",
+    dataIndex: "name",
+    key: "name",
+  },
+  {
+    title: "Capacidad",
+    dataIndex: "capacidad",
+    key: "capacidad",
+  },
+];
+
+const columns_matrix = [
+  {
+    title:
+      "Construye o adapta clases, módulos u otras piezas de software con ayuda de algún JR o SSR.",
+    dataIndex: "name",
+    key: "name",
+  },
+  {
+    title: "Aprendiz",
+    dataIndex: "capacidad",
+    key: "capacidad",
+  },
+];
+
+const columns_capa = [
+  {
+    title: "Link",
+    dataIndex: "link",
+    key: "link",
+    render: (text) => <a href={text}>Link</a>,
+  },
+  {
+    title: "Grupo",
+    dataIndex: "name",
+    key: "name",
+  },
+  {
+    title: "Tema",
+    dataIndex: "capacidad",
+    key: "capacidad",
+  },
+];
+
+const items = [
+  {
+    key: "1",
+    label: "Que hace?",
+    children: (
+      <>
+        <Table
+          pagination={false}
+          dataSource={desc}
+          columns={columns_desc}
+          size="small"
+        />
+      </>
+    ),
+  },
+  {
+    key: "2",
+    label: "Matriz de conocimientos",
+    children: (
+      <>
+        <Table
+          dataSource={matrix}
+          columns={columns_matrix}
+          pagination={false}
+          size="small"
+        />
+      </>
+    ),
+  },
+  {
+    key: "3",
+    label: "Capacidades a incorporar",
+    children: (
+      <>
+        <Table
+          pagination={false}
+          dataSource={capa}
+          columns={columns_capa}
+          size="small"
+        />
+      </>
+    ),
+  },
+];
+
+export default function Especialista() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1>Business System Teams</h1>
+      <h2>Especialista</h2>
+      <div>
+        <Timeline
+          items={[
+            {
+              children: (
+                <Card title="Dev Trainee">
+                  <Descriptions title="Descripcion"></Descriptions>
+                  <Collapse items={items} />
+                </Card>
+              ),
+            },
+            {
+              children: "Solve initial network problems 2015-09-01",
+            },
+            {
+              children: "Technical testing 2015-09-01",
+            },
+            {
+              children: "Network problems being solved 2015-09-01",
+            },
+          ]}
+        />
+      </div>
+    </main>
+  );
+}
--- carrer-path/src/app/bst/page.js
+++ carrer-path/src/app/bst/page.js
@@ -1,3 +1,38 @@
+"use client";
+import { Button, Space } from "antd";
+
 export default function FirstPost() {
-  return <h1>BST</h1>;
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1>Business System Teams</h1>
+      <Space direction="vertical" style={{ width: "100%" }}>
+        <Button
+          href="./bst/lider"
+          type="primary"
+          block
+          ghost
+          style={{ color: "red", borderColor: "red" }}
+        >
+          Lider
+        </Button>
+        <Button
+          type="primary"
+          href="./bst/especialista"
+          block
+          ghost
+          style={{ color: "red", borderColor: "red" }}
+        >
+          Especialista
+        </Button>
+        <Button
+          type="primary"
+          block
+          ghost
+          style={{ color: "red", borderColor: "red" }}
+        >
+          Negocios y gestión
+        </Button>
+      </Space>
+    </main>
+  );
 }
